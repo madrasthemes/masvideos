1824636f9036c02dec5681cf2ab33a1696a19cd2
Farook Ibrahim <farook@transvelo.com>
2018-10-17 20:12:31 +0530

    more formating functions added

--- includes/masvideos-formatting-functions.php
+++ includes/masvideos-formatting-functions.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * WooCommerce Formatting
+ * MasVideos Formatting
  *
  * Functions for formatting data.
  *
- * @package WooCommerce/Functions
- * @version 2.1.0
+ * @package MasVideos/Functions
+ * @version 1.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Converts a string (e.g. 'yes' or 'no') to a bool.
  *
- * @since 3.0.0
+ * @since 1.0.0
  * @param string $string String to convert.
  * @return bool
  */
@@ -24,7 +24,7 @@ function masvideos_string_to_bool( $string ) {
 /**
  * Converts a bool to a 'yes' or 'no'.
  *
- * @since 3.0.0
+ * @since 1.0.0
  * @param bool $bool String to convert.
  * @return string
  */
@@ -38,7 +38,7 @@ function masvideos_bool_to_string( $bool ) {
 /**
  * Explode a string into an array by $delimiter and remove empty values.
  *
- * @since 3.0.0
+ * @since 1.0.0
  * @param string $string    String to convert.
  * @param string $delimiter Delimiter, defaults to ','.
  * @return array
@@ -99,7 +99,7 @@ function masvideos_clean( $var ) {
 /**
  * Run masvideos_clean over posted textarea but maintain line breaks.
  *
- * @since  3.0.0
+ * @since  1.0.0
  * @param  string $var Data to sanitize.
  * @return string
  */
@@ -151,6 +151,140 @@ function masvideos_array_overlay( $a1, $a2 ) {
         }
     }
     return $a1;
+}
+
+/**
+ * MasVideos Date Format - Allows to change date format for everything MasVideos.
+ *
+ * @return string
+ */
+function masvideos_date_format() {
+    return apply_filters( 'masvideos_date_format', get_option( 'date_format' ) );
+}
+
+/**
+ * MasVideos Time Format - Allows to change time format for everything MasVideos.
+ *
+ * @return string
+ */
+function masvideos_time_format() {
+    return apply_filters( 'masvideos_time_format', get_option( 'time_format' ) );
+}
+
+/**
+ * Convert mysql datetime to PHP timestamp, forcing UTC. Wrapper for strtotime.
+ *
+ * Based on wcs_strtotime_dark_knight() from WC Subscriptions by Prospress.
+ *
+ * @since  1.0.0
+ * @param  string   $time_string    Time string.
+ * @param  int|null $from_timestamp Timestamp to convert from.
+ * @return int
+ */
+function masvideos_string_to_timestamp( $time_string, $from_timestamp = null ) {
+    $original_timezone = date_default_timezone_get();
+
+    // @codingStandardsIgnoreStart
+    date_default_timezone_set( 'UTC' );
+
+    if ( null === $from_timestamp ) {
+        $next_timestamp = strtotime( $time_string );
+    } else {
+        $next_timestamp = strtotime( $time_string, $from_timestamp );
+    }
+
+    date_default_timezone_set( $original_timezone );
+    // @codingStandardsIgnoreEnd
+
+    return $next_timestamp;
+}
+
+/**
+ * Convert a date string to a MasVideos_DateTime.
+ *
+ * @since  1.0.0
+ * @param  string $time_string Time string.
+ * @return MasVideos_DateTime
+ */
+function masvideos_string_to_datetime( $time_string ) {
+    // Strings are defined in local WP timezone. Convert to UTC.
+    if ( 1 === preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(Z|((-|\+)\d{2}:\d{2}))$/', $time_string, $date_bits ) ) {
+        $offset    = ! empty( $date_bits[7] ) ? iso8601_timezone_to_offset( $date_bits[7] ) : masvideos_timezone_offset();
+        $timestamp = gmmktime( $date_bits[4], $date_bits[5], $date_bits[6], $date_bits[2], $date_bits[3], $date_bits[1] ) - $offset;
+    } else {
+        $timestamp = masvideos_string_to_timestamp( get_gmt_from_date( gmdate( 'Y-m-d H:i:s', masvideos_string_to_timestamp( $time_string ) ) ) );
+    }
+    $datetime = new MasVideos_DateTime( "@{$timestamp}", new DateTimeZone( 'UTC' ) );
+
+    // Set local timezone or offset.
+    if ( get_option( 'timezone_string' ) ) {
+        $datetime->setTimezone( new DateTimeZone( masvideos_timezone_string() ) );
+    } else {
+        $datetime->set_utc_offset( masvideos_timezone_offset() );
+    }
+
+    return $datetime;
+}
+
+/**
+ * MasVideos Timezone - helper to retrieve the timezone string for a site until.
+ * a WP core method exists (see https://core.trac.wordpress.org/ticket/24730).
+ *
+ * Adapted from https://secure.php.net/manual/en/function.timezone-name-from-abbr.php#89155.
+ *
+ * @since 1.0.0
+ * @return string PHP timezone string for the site
+ */
+function masvideos_timezone_string() {
+    // If site timezone string exists, return it.
+    $timezone = get_option( 'timezone_string' );
+    if ( $timezone ) {
+        return $timezone;
+    }
+
+    // Get UTC offset, if it isn't set then return UTC.
+    $utc_offset = intval( get_option( 'gmt_offset', 0 ) );
+    if ( 0 === $utc_offset ) {
+        return 'UTC';
+    }
+
+    // Adjust UTC offset from hours to seconds.
+    $utc_offset *= 3600;
+
+    // Attempt to guess the timezone string from the UTC offset.
+    $timezone = timezone_name_from_abbr( '', $utc_offset );
+    if ( $timezone ) {
+        return $timezone;
+    }
+
+    // Last try, guess timezone string manually.
+    foreach ( timezone_abbreviations_list() as $abbr ) {
+        foreach ( $abbr as $city ) {
+            if ( (bool) date( 'I' ) === (bool) $city['dst'] && $city['timezone_id'] && intval( $city['offset'] ) === $utc_offset ) {
+                return $city['timezone_id'];
+            }
+        }
+    }
+
+    // Fallback to UTC.
+    return 'UTC';
+}
+
+/**
+ * Get timezone offset in seconds.
+ *
+ * @since  1.0.0
+ * @return float
+ */
+function masvideos_timezone_offset() {
+    $timezone = get_option( 'timezone_string' );
+
+    if ( $timezone ) {
+        $timezone_object = new DateTimeZone( $timezone );
+        return $timezone_object->getOffset( new DateTime( 'now' ) );
+    } else {
+        return floatval( get_option( 'gmt_offset', 0 ) ) * HOUR_IN_SECONDS;
+    }
 }
 
 /**
