3a4df54e97a9151a9c235cb7874bd5fe6e41378a
Farook Ibrahim <farook@transvelo.com>
2018-10-25 14:26:24 +0530

    attribute classes added

---
+++ includes/class-masvideos-movie-attribute.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Represents a attribute
+ *
+ * Attributes can be global (taxonomy based) or local to the itself.
+ * Uses ArrayAccess to be BW compatible with previous ways of reading attributes.
+ *
+ * @package MasVideos/Classes
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Attribute class.
+ */
+class MasVideos_Movie_Attribute extends MasVideos_Attribute {
+
+    /**
+     * Get taxonomy object.
+     *
+     * @return array|null
+     */
+    public function get_taxonomy_object() {
+        global $masvideos_attributes;
+        return $this->is_taxonomy() ? $masvideos_attributes['movie'][ $this->get_name() ] : null;
+    }
+}
