e1a475dfa72f27b740e068270518f6a65fe4558b
Farook Ibrahim <farook@transvelo.com>
2019-02-28 12:31:11 +0530

    playlist templates, form added

---
+++ templates/myaccount/edit-playlist.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Playlist Form
+ *
+ * This template can be overridden by copying it to yourtheme/masvideos/myaccount/edit-playlist.php.
+ *
+ * HOWEVER, on occasion MasVideos will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @package MasVideos/Templates
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+if( ! isset( $post_type ) ) {
+    return;
+}
+
+$id         = 0;
+$title      = '';
+$visibility = '';
+
+if( ! empty( $playlist ) ) {
+    $id         = $playlist->get_id();
+    $title      = $playlist->get_name();
+    $visibility = $playlist->get_status();
+}
+
+do_action( 'masvideos_before_edit_playlist_form', $playlist ); ?>
+
+<div class="masvideos-edit-playlist">
+    <div class="masvideos-edit-playlist__inner">
+
+        <h2><?php esc_html_e( 'Create Playlist', 'masvideos' ); ?></h2>
+
+        <form method="post" class="masvideos-form masvideos-edit-playlist__form masvideos-edit-playlist__form--<?php esc_attr_e( $post_type ); ?>" <?php do_action( 'masvideos_edit_playlist_form_tag', $playlist ); ?> >
+
+            <?php do_action( 'masvideos_edit_playlist_form_start', $playlist ); ?>
+
+            <p class="masvideos-form-row masvideos-form-row--wide form-row form-row-wide">
+                <label for="<?php esc_attr_e( $post_type ); ?>-title"><?php esc_html_e( 'Title', 'masvideos' ); ?>&nbsp;<span class="required">*</span></label>
+                <input type="text" class="masvideos-Input masvideos-Input--text input-text" name="title" id="<?php esc_attr_e( $post_type ); ?>-title" value="<?php echo esc_attr( $title ); ?>" /><?php // @codingStandardsIgnoreLine ?>
+            </p>
+
+            <p class="masvideos-form-row masvideos-form-row--wide form-row form-row-wide">
+                <label for="<?php esc_attr_e( $post_type ); ?>-visibility"><?php esc_html_e( 'Visibility', 'masvideos' ); ?>&nbsp;<span class="required">*</span></label>
+                <select class="masvideos-Input masvideos-Input--select input-select" name="visibility" id="<?php esc_attr_e( $post_type ); ?>-visibility">
+                    <?php foreach ( masvideos_get_movie_playlist_visibility_options() as $key => $value ) : ?>
+                        <option value="<?php echo esc_attr( $key ); ?>" <?php selected( $visibility, $key ); ?>><?php echo esc_attr( $value ); ?></option><?php // @codingStandardsIgnoreLine ?>
+                    <?php endforeach; ?>
+                </select>
+            </p>
+
+            <?php do_action( 'masvideos_edit_playlist_form', $playlist ); ?>
+
+            <p class="masvideos-FormRow form-row">
+                <input name="id" type="hidden" value="<?php echo esc_attr( $id ); ?>" />
+                <input name="post_type" type="hidden" value="<?php echo esc_attr( $post_type ); ?>" />
+                <?php wp_nonce_field( 'masvideos-edit-playlist', 'masvideos-edit-playlist-nonce' ); ?>
+                <button type="submit" class="masvideos-Button button" name="edit-playlist" value="<?php esc_attr_e( 'Create', 'masvideos' ); ?>"><?php esc_html_e( 'Create', 'masvideos' ); ?></button>
+            </p>
+
+            <?php do_action( 'masvideos_edit_playlist_form_end', $playlist ); ?>
+
+        </form>
+    </div>
+</div>
+
+<?php do_action( 'masvideos_after_edit_playlist_form', $playlist ); ?>
