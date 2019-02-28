b1d318df0d017d30ab4b66ed987fbcfa3b8815e3
Farook Ibrahim <farook@transvelo.com>
2019-02-28 13:02:38 +0530

    playlsit manage sc added

--- includes/shortcodes/class-masvideos-shortcode-my-account.php
+++ includes/shortcodes/class-masvideos-shortcode-my-account.php
@@ -47,4 +47,33 @@ class MasVideos_Shortcode_My_Account {
             masvideos_get_template( 'myaccount/form-login.php' );
         }
     }
+
+    /**
+     * Output the shortcode.
+     *
+     * @param array $atts Shortcode attributes.
+     */
+    public static function manage_playlists( $atts ) {
+        if ( is_user_logged_in() ) {
+            $post_type = ! empty( $atts['post_type'] ) ? $atts['post_type'] : 'movie_playlist';
+
+            if( isset( $_GET['post'] ) && isset( $_GET['action'] ) && $_GET['action'] == 'edit' ) {
+                $id = isset( $_GET['post'] ) ? absint( $_GET['post'] ) : absint( get_query_var( 'post', 0 ) ); // WPCS: sanitization ok, input var ok, CSRF ok.
+                $playlist_func_name = 'masvideos_get_' . $post_type;
+                $playlist = function_exists( $playlist_func_name ) ? $playlist_func_name( $id ) : false;
+            }
+
+            masvideos_get_template( 'myaccount/edit-playlist.php', array(
+                'post_type'     => $post_type,
+                'playlist'      => isset( $playlist ) && $playlist ? $playlist : false,
+            ) );
+
+            $playlists_func_name = 'masvideos_get_current_user_' . $post_type . 's';
+
+            masvideos_get_template( 'myaccount/manage-playlists.php', array(
+                'post_type'     => $post_type,
+                'playlists'     => function_exists( $playlists_func_name ) ? $playlists_func_name() : false
+            ) );
+        }
+    }
 }
