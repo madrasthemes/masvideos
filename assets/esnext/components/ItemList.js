d76b0c52f9ef8e46cffb49cb1db18df68591d430
Yusuf Ansari <yusuf@transvelo.com>
2019-04-09 17:36:22 +0530

    gutenberg block edit

--- assets/esnext/components/ItemList.js
+++ assets/esnext/components/ItemList.js
@@ -12,23 +12,23 @@ export const ItemList = props => {
     const { filtered = false, loading = false, items = [], action = () => {}, icon = null } = props;
 
     if (loading) {
-        return <p className="loading-items">{__('Loading ...', 'masvideos')}</p>;
+        return <p className="loading-items">{__('Loading ...', 'vodi')}</p>;
     }
 
     if (filtered && items.length < 1) {
         return (
             <div className="item-list">
-                <p>{__('Your query yielded no results, please try again.', 'masvideos')}</p>
+                <p>{__('Your query yielded no results, please try again.', 'vodi')}</p>
             </div>
         );
     }
 
     if ( ! items || items.length < 1 ) {
-        return <p className="no-items">{__('Not found.', 'masvideos')}</p>
+        return <p className="no-items">{__('Not found.', 'vodi')}</p>
     }
 
     return (
-        <div class="item-list">
+        <div className="item-list">
             {items.map((item) => <Item key={item.id} {...item} clickHandler={action} icon={icon} />)}
         </div>
     );
