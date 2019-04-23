9bad0602fbb96111ac379978077bb01d950463af
Farook Ibrahim <farook@transvelo.com>
2019-04-23 17:47:37 +0530

    sources displayed in single tabs

---
+++ templates/single-episode/sources.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Sources Template
+ *
+ * This template can be overridden by copying it to yourtheme/masvideos/single-episode/sources.php.
+ *
+ * HOWEVER, on occasion MasVideos will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @package MasVideos/Templates
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+global $episode;
+
+if ( ! $episode || ! ( $episode->has_sources() ) ) {
+    return;
+}
+
+$sources = $episode->get_sources();
+
+?>
+<table>
+    <thead>
+        <tr>
+            <th><?php echo esc_html__( 'Links', 'masvideos' ) ?></th>
+            <th><?php echo esc_html__( 'Quality', 'masvideos' ) ?></th>
+            <th><?php echo esc_html__( 'Language', 'masvideos' ) ?></th>
+            <th><?php echo esc_html__( 'Player', 'masvideos' ) ?></th>
+            <th><?php echo esc_html__( 'Date Added', 'masvideos' ) ?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ( $sources as $key => $source ) : ?>
+            <?php
+                $source_src = '';
+                $source_choice = ! empty( $source['choice'] ) ? $source['choice'] : '';
+
+                if ( $source_choice == 'episode_embed' && ! empty( $source['embed_content'] ) ) {
+                    $source_src = $source['embed_content'];
+                } elseif ( $source_choice == 'episode_url' && ! empty( $source['link'] ) ) {
+                    $source_src = $source['link'];
+                }
+
+                if( empty( $source_src ) ) {
+                    continue;
+                }
+            ?>
+            <tr>
+                <td>
+                    <a href="#" class="play-source-link">
+                        <span><?php echo esc_html__( 'Play Now', 'masvideos' ) ?></span>
+                    </a>
+                </td>
+                <td>
+                    <?php if( ! empty( $source['quality'] ) ) {
+                        echo wp_kses_post( $source['quality'] );
+                    } ?>
+                </td>
+                <td>
+                    <?php if( ! empty( $source['language'] ) ) {
+                        echo wp_kses_post( $source['language'] );
+                    } ?>
+                </td>
+                <td>
+                    <?php if( ! empty( $source['player'] ) ) {
+                        echo wp_kses_post( $source['player'] );
+                    } ?>
+                </td>
+                <td>
+                    <?php if( ! empty( $source['date_added'] ) ) {
+                        echo wp_kses_post( $source['date_added'] );
+                    } ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
