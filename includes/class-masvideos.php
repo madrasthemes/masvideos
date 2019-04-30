e8e05985b3061d5c8c2a80bbeb2e0cd43833cf12
Farook Ibrahim <farook@transvelo.com>
2019-04-30 14:06:14 +0530

    version, masvideos.pot updated

--- includes/class-masvideos.php
+++ includes/class-masvideos.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'MasVideos' ) ) {
          *
          * @var string
          */
-        public $version = '1.0.0';
+        public $version = '1.0.1';
 
         /**
          * The single instance of the class.
