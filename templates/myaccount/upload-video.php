5e055fae698114f96ea476e528c82b4e64d513ea
Farook Ibrahim <farook@transvelo.com>
2019-05-17 15:24:53 +0530

    upload-video template added

--- templates/myaccount/upload-video.php
+++ templates/myaccount/upload-video.php
@@ -20,3 +20,33 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+do_action( 'masvideos_before_edit_upload_video_form' ); ?>
+
+<form method="post">
+
+    <h3><?php echo apply_filters( 'masvideos_my_account_upload_video_title', __( 'Upload video', 'masvideos' ) ); ?></h3><?php // @codingStandardsIgnoreLine ?>
+
+    <div class="masvideos-upload-video-fields">
+        <?php do_action( "masvideos_before_edit_upload_video" ); ?>
+
+        <div class="masvideos-upload-video-fields__field-wrapper">
+            <?php
+            $fields = masvideos_get_upload_video_fields();
+            foreach ( $fields as $key => $field ) {
+                masvideos_form_field( $key, $field, masvideos_get_post_data_by_key( $key, $field['value'] ) );
+            }
+            ?>
+        </div>
+
+        <?php do_action( "masvideos_after_edit_upload_video" ); ?>
+
+        <p>
+            <button type="submit" class="button" name="save_video" value="<?php esc_attr_e( 'Submit video', 'masvideos' ); ?>"><?php esc_html_e( 'Submit video', 'masvideos' ); ?></button>
+            <?php wp_nonce_field( 'masvideos-upload_video', 'masvideos-upload-video-nonce' ); ?>
+            <input type="hidden" name="action" value="upload_video" />
+        </p>
+    </div>
+
+</form>
+
+<?php do_action( 'masvideos_after_edit_upload_video_form' ); ?>
