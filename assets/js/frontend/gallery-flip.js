6143f8788cb0bd0623be14a0d3ec6c5a1bd6ddb1
Farook Ibrahim <farook@transvelo.com>
2020-03-03 11:36:46 +0530

    Fix: #35

--- assets/js/frontend/gallery-flip.js
+++ assets/js/frontend/gallery-flip.js
@@ -7,22 +7,24 @@
         /*===================================================================================*/
         $( '.videos .video__poster' ).each( function() {
             var $this = $( this ).find( '.video__poster--image' );
-            var imagesjson = $this.attr( 'data-gallery-images' );
-            var images = JSON.parse( imagesjson );
-            var original_src = $this.attr( 'src' );
-            var original_srcset = $this.attr( 'srcset' );
-            var shouldRotateThumbnails = false;
+            if ( typeof $this.data('gallery-images') !== 'undefined' ) {
+                var imagesjson = $this.attr( 'data-gallery-images' );
+                var images = JSON.parse( imagesjson );
+                var original_src = $this.attr( 'src' );
+                var original_srcset = $this.attr( 'srcset' );
+                var shouldRotateThumbnails = false;
 
-            $( this ).on( 'mouseenter', function() {
-                shouldRotateThumbnails = true;
-                rotateThumbnails();
-            });
+                $( this ).on( 'mouseenter', function() {
+                    shouldRotateThumbnails = true;
+                    rotateThumbnails();
+                });
 
-            $( this ).on( 'mouseleave', function() {
-                shouldRotateThumbnails = false;
-                $this.attr( 'src', original_src );
-                $this.attr( 'srcset', original_srcset );
-            });
+                $( this ).on( 'mouseleave', function() {
+                    shouldRotateThumbnails = false;
+                    $this.attr( 'src', original_src );
+                    $this.attr( 'srcset', original_srcset );
+                });
+            }
 
             function rotateThumbnails() {
                 if( images.length > 0 ) {
@@ -46,4 +48,4 @@
             }
         } );
     } );
-} ) ( jQuery, window ) ;
+} ) ( jQuery, window );
