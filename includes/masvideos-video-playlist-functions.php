05a793b17a9d9c93feebef22ded8e10deb1c18eb
jmabbas <mdabbas.it@gmail.com>
2025-03-06 14:00:07 +0530

    playlist name update

--- includes/masvideos-video-playlist-functions.php
+++ includes/masvideos-video-playlist-functions.php
@@ -110,7 +110,7 @@ function masvideos_delete_video_playlist_transients( $post_id = 0 ) {
  * @param  array $args Playlist arguments.
  * @return int|WP_Error
  */
-function masvideos_update_video_playlist( $args, $id = 0) {
+function masvideos_update_video_playlist( $id = 0,$args=[]) {
     $video_playlist = masvideos_get_video_playlist( $id );
 
     if ( ! $video_playlist ) {
